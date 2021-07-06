d114cd3932c82678181bf00433e6c63807de3611
xasadr <xasadr@gmail.com>
2021-07-06 00:53:01 -0400

    added image upload to show on main screen

--- src/components/objectDetector/index.jsx
+++ src/components/objectDetector/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
 import "@tensorflow/tfjs-backend-cpu";
@@ -14,7 +14,7 @@ const ObjectDetectorContainer = styled.div`
 
 const DetectorContainer = styled.div`
     min-width: 200px;
-    height: 500px;
+    height: 800px;
     border: 3px solid #fff;
     border-radius: 5px;
     display: flex;
@@ -54,15 +54,32 @@ const SelectButton = styled.button`
 export function ObjectDetector(props) {
 
     const fileInputRef = useRef();
+    const [imgData, setImgData] = useState(null);
 
     const openFilePicker = () => {
         if(fileInputRef.current) fileInputRef.current.click();
     };
 
+    const readImage = async (file) => {
+        return new Promise((rs,rj) => {
+            const fileReader = new FileReader();
+            fileReader.onload = () => rs(fileReader.result);
+            fileReader.onerror = () => rj(fileReader.error);
+            fileReader.readAsDataURL(file);
+        })
+    }
+
+    const onSelectImage = async (e) => {
+        const file = e.target.files[0];
+        const imgData = await readImage(file);
+        setImgData(imgData);
+    }
     return (
         <ObjectDetectorContainer>
-            <DetectorContainer>Img</DetectorContainer>
-            <HiddenFileInput type = "file" ref={fileInputRef}/>
+            <DetectorContainer>
+                {imgData && <TargetImg src={imgData} />}
+            </DetectorContainer>
+            <HiddenFileInput type = "file" ref={fileInputRef} onChange={onSelectImage}/>
             <SelectButton onClick={openFilePicker}>Select Image</SelectButton>
         </ObjectDetectorContainer>
     );
